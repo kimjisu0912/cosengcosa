84c5ab809e3ae206f60feb4bc213628dc5113e12
jsk <jsk@DESKTOP-H24D234>
2022-12-31 22:50:55 +0900

    테스트2

--- cosengcosa/src/main/java/com/cosengcosa/room/controller/ClassMainController.java
+++ cosengcosa/src/main/java/com/cosengcosa/room/controller/ClassMainController.java
@@ -15,7 +15,7 @@ import com.cosengcosa.room.service.ClassMainService;
  * @author 김지수/박예준
  * 
  */
-
+ 
 @Controller
 public class ClassMainController {
 
@@ -39,7 +39,7 @@ public class ClassMainController {
 		// 메인강의 리스트 서비스 호출통해서 메인강의 리스트 관련 데이터를 가져온다
 		Map<String, Object> modelMap = classMainService.classMainList(pageNum, type, keyword);
 		
-		// 파라미터로 받은 모델 객체에 도메인 객체나 비지니스 로직을 처리한 결과를 모델을 저장한다.
+		// 파라미터로 받은 모델 객체에 도메인 객체나 비지니스 로직을 처리한 결과를 모델을 저장.
 		model.addAllAttributes(modelMap);
 		
 		return "classMainList";
