a6b70ac06d715bf60c7e12ad0655a68d78379e03
15B <15B@DESKTOP-GQ6GUL5>
2023-01-05 17:44:03 +0900

    오라클 쿼리 샘플 수정 cosengcosa_oracle.sql by 김지수

--- cosengcosa/src/main/resources/SQL/cosengcosa_oracle.sql
+++ cosengcosa/src/main/resources/SQL/cosengcosa_oracle.sql
@@ -55,12 +55,12 @@ CREATE TABLE classmain(
         CONSTRAINT CM_YN_NN NOT NULL                    -- 삭제유무
 );
 
-select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a;
+select 'cm'||TO_CHAR(nvl(max(a.cm_no)+1,1)) from classmain a;
 
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
 VALUES 
 (classmain_seq.NEXTVAL,
-(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a),
+(select 'cm'||TO_CHAR(nvl(max(a.cm_no)+1,1)) from classmain a),
 'ja01', 
 '자바를 잡아라~!', 
 '2017-12-01 09:00:00', 
@@ -78,71 +78,63 @@ null,
 -- 메인강의 테이블 값 입력
 -- 적당히 한 70번만 컨트롤 시프트 엔터로 하자
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'ja01', '자바를 잡아라~!', '2017-12-01 09:00:00', '2017-12-20 12:00:00', 35000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'ja01', '자바를 잡아라~!', '2017-12-01 09:00:00', '2017-12-20 12:00:00', 35000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'ja02', '자바를 내친구~!', '2018-01-01 09:00:00', '2018-01-20 12:00:00', 30000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'ja02', '자바를 내친구~!', '2018-01-01 09:00:00', '2018-01-20 12:00:00', 30000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'ja03', '자바의 정석', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'ja03', '자바의 정석', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'DB01', 'SQL 문법을 이해하자~!', '2017-12-01 09:00:00', '2017-12-20 12:00:00', 55000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'DB01', 'SQL 문법을 이해하자~!', '2017-12-01 09:00:00', '2017-12-20 12:00:00', 55000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'DB02', 'MySQL활용', '2018-01-01 09:00:00', '2018-01-20 12:00:00', 30000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'DB02', 'MySQL활용', '2018-01-01 09:00:00', '2018-01-20 12:00:00', 30000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'DB03', '마리아 디비의 학습', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'DB03', '마리아 디비의 학습', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'jq01', 'Jquery의 학습', '2017-12-01 09:00:00', '2017-12-20 12:00:00', 55000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'jq01', 'Jquery의 학습', '2017-12-01 09:00:00', '2017-12-20 12:00:00', 55000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'js01', 'JavaScript 활용', '2018-01-01 09:00:00', '2018-01-20 12:00:00', 30000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'js01', 'JavaScript 활용', '2018-01-01 09:00:00', '2018-01-20 12:00:00', 30000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'HL01', 'HTML5학습', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'HL01', 'HTML5학습', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'cs01', 'css3의 기초지식', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'cs01', 'css3의 기초지식', '2018-02-01 09:00:00', '2018-02-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'jp01', 'JSP 2.0', '2018-03-01 09:00:00', '2018-03-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'jp01', 'JSP 2.0', '2018-03-01 09:00:00', '2018-03-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
 INSERT INTO classmain (cm_no, cm_id, cm_code, cm_name, cm_sdate, cm_edate, cm_price, cm_count, cm_recommend, cm_star, file1, video, cm_yn) 
-VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(max(a.cm_no) + 1) from classmain a), 'sp01', 'spring4', '2018-04-01 09:00:00', '2018-04-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
+VALUES (classmain_seq.NEXTVAL,(select 'cm'||TO_CHAR(NVL(max(a.cm_no)+1 ,1)) from classmain a), 'sp01', 'spring4', '2018-04-01 09:00:00', '2018-04-20 12:00:00', 45000, 1, 1, 4, null, null,'Y');
 
 commit;
 select * from classmain ORDER BY cm_no DESC;
 
 -- 검색쿼리
 SELECT
-	cm_no AS cmNo,
-    cm_id AS cmId,
-    cm_code AS cmCode,
-    cm_name AS cmName,
-    cm_sdate AS cmSdate,
-    cm_edate AS cmEdate,
-    cm_price AS cmPrice,
-    cm_count AS cmCount,
-    cm_recommend AS cmRecommend,
-    cm_star AS cmStar,
-    file1 AS file1,
-    video AS video,
-    cm_yn AS cmYn
-FROM
-	classmain
-WHERE
- cm_yn = "Y"
-AND cm_name LIKE CONCAT('%', "", '%')
-ORDER BY cmNo DESC
-LIMIT 1, 10;
-
--- 이건 쿼리 테스트 한거임
-SELECT concat('cm','',MAX(a.cm_no)) FROM classmain a;
-(select concat('cm','',ifnull(max(a.cm_no), 0)+1) from classmain a);
-
-
-SELECT 
-			no,
-			title,
-			writer,
-			content,
-			reg_date AS regDate,
-			read_count AS readCount,
-			pass,
-			file1
-		FROM springbbs
-        WHERE
-        title LIKE CONCAT('%', "", '%')
-        ;
+	    num, cmNo, cmId, cmCode, cmName, cmSdate, cmEdate, cmPrice, cmCount,
+	    cmRecommend, cmStar, file1, video, cmYn
+	FROM    
+	    (SELECT
+	        ROWNUM num, cmNo, cmId, cmCode, cmName, cmSdate, cmEdate, cmPrice, cmCount,
+	        cmRecommend, cmStar, file1, video, cmYn
+	    FROM 
+	        (SELECT
+	            cm_no AS cmNo,
+	            cm_id AS cmId,
+	            cm_code AS cmCode,
+	            cm_name AS cmName,
+	            cm_sdate AS cmSdate,
+	            cm_edate AS cmEdate,
+	            cm_price AS cmPrice,
+	            cm_count AS cmCount,
+	            cm_recommend AS cmRecommend,
+	            cm_star AS cmStar,
+	            file1 AS file1,
+	            video AS video,
+	            cm_yn AS cmYn
+	        FROM
+            classmain
+			WHERE
+            cm_yn = 'Y'
+            AND cm_name LIKE '%%'
+        ) classmain
+        ORDER BY cmNo DESC
+    )
+	WHERE num>=1 AND num <= 10
+    ;
