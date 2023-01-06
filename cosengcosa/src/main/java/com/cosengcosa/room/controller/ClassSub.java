b99e23bcb917db72eceb7d6b315f2c4137579a13
15B <15B@DESKTOP-GQ6GUL5>
2023-01-06 18:26:06 +0900

    강의서브 작업 1 by 김지수

---
+++ cosengcosa/src/main/java/com/cosengcosa/room/controller/ClassSub.java
@@ -0,0 +1,19 @@
+package com.cosengcosa.room.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 서브강의 컨트롤러
+ * @author 김지수
+ *
+ */
+
+@Controller
+public class ClassSub {
+
+	// 서브강의 서비스 생성자 생성
+	
+	/*
+	 * 서브강의 리스트
+	 */
+}
---
+++ cosengcosa/src/main/java/com/cosengcosa/room/domain/ClassSub.java
@@ -0,0 +1,24 @@
+package com.cosengcosa.room.domain;
+
+import java.sql.Timestamp;
+
+/**
+ * 서브강의
+ * @author 김지수
+ *
+ */
+
+public class ClassSub {
+	
+	private int csNo;
+	private String csId;
+	private String csCode;
+	private String csGroup;
+	private String csName;
+	private Timestamp csSdate;
+	private Timestamp csEdate;
+	private int csCount;
+	private int csRecommend;
+	private int csStar;
+	
+}
