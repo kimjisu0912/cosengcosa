6628288c7e9e1c34b4c7a05b7d3b71a2f524734f
kdy <kdy@192.168.219.110>
2023-01-10 17:45:12 +0900

    로그인 및 회원가입 구현

--- cosengcosa/src/main/java/com/cosengcosa/room/controller/MainController.java
+++ cosengcosa/src/main/java/com/cosengcosa/room/controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
+/** 
  * 메인페이지 컨트롤러
  * @author 김동영
  * 
